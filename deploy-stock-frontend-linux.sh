6fd833da118b1b5a2a121269ff3e6cc390e31f6f
andold <andold@naver.com>
2025-09-16 18:49:19 +0900

    support deploy scripts

---
+++ deploy-stock-frontend-linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+#
+PROJECT=stock-frontend
+PROFILE=linux
+HOME_DIR=/home/andold
+INSTALL_SCRIPT_FILE_NAME=install-$PROJECT-$PROFILE.sh
+DEPLOY_SCRIPT_FILE_NAME=deploy-$PROJECT-$PROFILE.sh
+SOURCE_DIR=$HOME_DIR/src/github/$PROJECT
+DEPLOY_DIR=$HOME_DIR/deploy/apache2
+#
+#
+# copy install script file
+#
+cp $SOURCE_DIR/$INSTALL_SCRIPT_FILE_NAME $DEPLOY_DIR
+chmod a+x $DEPLOY_DIR/$INSTALL_SCRIPT_FILE_NAME
+# 
+# 
+# copy build 
+#
+rm -fr $DEPLOY_DIR/doc_base/anonymous/stock/*
+cp -fr $SOURCE_DIR/build/* $DEPLOY_DIR/doc_base/anonymous/stock/
+#
+#
+# apache2 재시작
+#
+sudo apachectl restart
+#
+#
+#
+#
+jps
+sudo apchectl status
+#
---
+++ install-stock-frontend-linux.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#
+PROJECT=stock-frontend
+PROFILE=linux
+HOME_DIR=/home/andold
+INSTALL_SCRIPT_FILE_NAME=install-$PROJECT-$PROFILE.sh
+DEPLOY_SCRIPT_FILE_NAME=deploy-$PROJECT-$PROFILE.sh
+SOURCE_DIR=$HOME_DIR/src/github/$PROJECT
+DEPLOY_DIR=$HOME_DIR/deploy/apache2
+#
+#
+date
+#
+#
+# source checkout
+#
+git config --global core.quotepath false
+cd	$SOURCE_DIR
+git config pull.rebase false
+git config pull.ff only
+git stash
+git clean -f
+git pull
+git log --pretty=format:"%h - %an, %ai:%ar : %s" -8
+#
+#
+# copy deploy script file
+#
+cp $SOURCE_DIR/$DEPLOY_SCRIPT_FILE_NAME	$DEPLOY_DIR
+#
+#
+cd $DEPLOY_DIR
+chmod a+x $DEPLOY_SCRIPT_FILE_NAME
+bash $DEPLOY_SCRIPT_FILE_NAME
+#
