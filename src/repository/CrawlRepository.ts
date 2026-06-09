bef7ed02183bf4311ff9542ead2a808c9d8b73ad
andold <andold@naver.com>
2026-06-09 15:58:25 +0900

    API 변경사항 적용 /api/crawl/item/ipo-close/recent

--- src/repository/CrawlRepository.ts
+++ src/repository/CrawlRepository.ts
@@ -32,7 +32,7 @@ class CrawlRepository {
 			.catch(error => onError && onError(request, error, element));
 	}
 	async crawlItemIpoCloseRecent(request: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.get("./api/crawl/item/ipo-close/recent", request)
+		return axios.post("./api/crawl/item/ipo-close/recent", request)
 			.then(response => onSuccess && onSuccess(request, response.data, element))
 			.catch(error => onError && onError(request, error, element));
 	}
